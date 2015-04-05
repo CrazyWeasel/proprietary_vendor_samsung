96960508b14ecb06d66275340b91d6b2f9abe689
Dheeraj CVR <cvr.dheeraj@gmail.com>
2015-04-06 01:36:30 +0400

    Revert "t0lte/i9305: use prebuilt gps hal for now"
    This reverts commit 889d60e3c87fb92cea55e72b18ef778d141a28fd.
    
    Change-Id: I4e1e60ae8fbce13ba613cbf159f35c7b6de35c96

--- i9305/i9305-vendor-blobs.mk
+++ i9305/i9305-vendor-blobs.mk
@@ -64,9 +64,3 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/system/usr/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/proprietary/system/lib/hw/gps.default.so:/system/lib/hw/gps.default.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libgps.utils.so:/system/lib/libgps.utils.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libloc_adapter.so:/system/lib/libloc_adapter.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libloc_eng.so:/system/lib/libloc_eng.so
